91567f852f70fedad1f6dbf0ede77420b339800f
KamSaf <bopaf42@gmail.com>
2023-10-24 12:38:35 +0200

    initial commit

---
+++ about.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <title>PHP Blog</title>
+</head>
+<header>
+<?php include($_SERVER['DOCUMENT_ROOT']. '/blog/templates/header.html'); ?>
+</header>
+<body>
+    
+<center>
+    <h1 style="margin-top: 150px; margin-bottom: 25px;">Welcome to the Cat enthusiasts forum! 🐈</h1>
+    <span>
+        If you feel the urge to show off your cat to the world then you are in the right place!</br> 
+        Here you can post pictures of your pet and comment on other users photos!
+    </span>
+</center>
+    <?php include($_SERVER['DOCUMENT_ROOT']. '/blog/templates/footer.html'); ?>
+</body>
+</html>
+
+
---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <title>PHP Blog</title>
+</head>
+<header>
+<?php include($_SERVER['DOCUMENT_ROOT']. '/blog/templates/header.html'); ?>
+</header>
+<body>
+    
+    <center><h1 style="margin-top: 100px;">Log in page!</h1></center>
+    <?php include($_SERVER['DOCUMENT_ROOT']. '/blog/templates/footer.html'); ?>
+</body>
+</html>
+
+
