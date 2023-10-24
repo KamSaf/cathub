92cc3d0326b7e57a8a4e6b11e319b8de22642e47
KamSaf <bopaf42@gmail.com>
2023-10-24 15:17:30 +0200

    changed name to CatHub!

--- about.php
+++ about.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
-    <title>PHP Blog</title>
+    <title>CatHub 🐱</title>
 </head>
 <header>
 <?php include($_SERVER['DOCUMENT_ROOT']. '/blog/templates/header.html'); ?>
@@ -13,7 +13,7 @@
 <body>
     
 <center>
-    <h1 style="margin-top: 150px; margin-bottom: 25px;">Welcome to the Cat enthusiasts forum! 🐈</h1>
+    <h1 style="margin-top: 150px; margin-bottom: 25px;">Welcome to the CatHub! 🐈</h1>
     <span>
         If you feel the urge to show off your cat to the world then you are in the right place!</br> 
         Here you can post pictures of your pet and comment on other users photos!
