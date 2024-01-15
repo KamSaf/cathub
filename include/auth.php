b0166ab07e11a774fe44e9e7bdcb5c0fa3a17bd0
KamSaf <bopaf42@gmail.com>
2024-01-15 22:04:36 +0100

    Added PDO to auth.php

--- include/auth.php
+++ include/auth.php
@@ -1,15 +1,41 @@
 <?php
+
+    function start_pdo_conn(){
+        return new PDO ('mysql:host=localhost;dbname=cathub','root','');
+    }
+
     require_once($_SERVER['DOCUMENT_ROOT']. '/cathub/include/database.php');
     session_start();
 
+    // # Checks if user with provided email and password exists, if not returns -1 (OLD)
+    // function user_login(string $email, string $password){
+    //     $conn = new PDO ('mysql:host=localhost;dbname=studenci_zajecia','root','');
+    //     $active_users_query = "SELECT * FROM users WHERE email = '{$email}' AND is_deleted = false";
+    //     $users = mysqli_query($conn, $active_users_query);
+    //     if (mysqli_num_rows($users) > 0)
+    //         $user = mysqli_fetch_assoc($users);
+    //         mysqli_close($conn);
+    //         if (password_verify($password, $user['password'])){
+    //             session_start();
+    //             $_SESSION['logged'] = true;
+    //             $_SESSION['logged_user'] = $user;
+    //             header("Location: home.php");
+    //             exit;
+    //         }
+    //     mysqli_close($conn);
+    //     return -1;
+    // }
+
     # Checks if user with provided email and password exists, if not returns -1
     function user_login(string $email, string $password){
-        $conn = open_db_connection();
-        $active_users_query = "SELECT * FROM users WHERE email = '{$email}' AND is_deleted = false";
-        $users = mysqli_query($conn, $active_users_query);
-        if (mysqli_num_rows($users) > 0)
-            $user = mysqli_fetch_assoc($users);
-            mysqli_close($conn);
+        $conn = start_pdo_conn();
+        $active_users_query = "SELECT * FROM users WHERE email = :email AND is_deleted = false";
+        $prep = $conn->prepare($active_users_query);
+        $prep->bindParam(':email', $email, PDO::PARAM_STR);
+        $prep->execute();
+        $user = $prep->fetch(PDO::FETCH_ASSOC);
+        $conn = null;
+        if ($user){
             if (password_verify($password, $user['password'])){
                 session_start();
                 $_SESSION['logged'] = true;
@@ -17,48 +43,89 @@
                 header("Location: home.php");
                 exit;
             }
-        mysqli_close($conn);
+        }
         return -1;
     }
 
+    // # Checks if provided email address is available to use (OLD)
+    // function check_if_email_used(mysqli $conn, string $email){
+    //     $used_emails_query = "SELECT email FROM users WHERE is_deleted = false AND email = '{$email}'";
+    //     $query_result = mysqli_query($conn, $used_emails_query);
+    //     if (mysqli_num_rows($query_result) > 0)
+    //         return true;
+    //     return true;
+    // }
+
     # Checks if provided email address is available to use
-    function check_if_email_used(mysqli $conn, string $email){
-        $used_emails_query = "SELECT email FROM users WHERE is_deleted = false AND email = '{$email}'";
-        $query_result = mysqli_query($conn, $used_emails_query);
-        if (mysqli_num_rows($query_result) > 0)
+    function check_if_email_used(string $email){
+        $conn = start_pdo_conn();
+        $used_emails_query = "SELECT * FROM users WHERE is_deleted = false AND email = :email";
+        $prep = $conn->prepare($used_emails_query);
+        $prep->bindParam(':email', $email, PDO::PARAM_STR);
+        $prep->execute();
+        $users = $prep->fetchAll(PDO::FETCH_ASSOC);
+        $conn = null;
+        if (count($users) > 0)
             return true;
         return false;
     }
 
-    # Checks if provided username is available to use
-    function check_if_username_used(mysqli $conn, string $username){
-        $used_usernames_query = "SELECT username FROM users WHERE is_deleted = false AND username = '{$username}'";
-        $query_result = mysqli_query($conn, $used_usernames_query);
-        if (mysqli_num_rows($query_result) > 0)
+    // # Checks if provided username is available to use (OLD)
+    // function check_if_username_used(mysqli $conn, string $username){
+    //     $used_usernames_query = "SELECT username FROM users WHERE is_deleted = false AND username = '{$username}'";
+    //     $query_result = mysqli_query($conn, $used_usernames_query);
+    //     if (mysqli_num_rows($query_result) > 0)
+    //         return true;
+    //     return false;
+    // }
+
+    # Checks if provided username is available to use (OLD)
+    function check_if_username_used(string $username){
+        $conn = start_pdo_conn();
+        $used_usernames_query = "SELECT username FROM users WHERE is_deleted = false AND username = :username";
+        $prep = $conn->prepare($used_usernames_query);
+        $prep->bindParam(':username', $username, PDO::PARAM_STR);
+        $prep->execute();
+        $users = $prep->fetchAll(PDO::FETCH_ASSOC);
+        $conn = null;
+        if (count($users) > 0)
             return true;
         return false;
     }
+
+    // # Creates user in the database (OLD)
+    // function create_user(string $username, string $email, string $password){
+    //     $conn = open_db_connection();
+    //     $password_hash = password_hash($password, PASSWORD_DEFAULT);
+    //     $insert_user = "INSERT INTO users (username, password, email) VALUES ('{$username}', '{$password_hash}', '{$email}')";
+    //     mysqli_query($conn, $insert_user);
+    //     mysqli_close($conn);
+    //     user_login($email, $password);
+    // }
 
     # Creates user in the database
     function create_user(string $username, string $email, string $password){
-        $conn = open_db_connection();
+        $conn = start_pdo_conn();
         $password_hash = password_hash($password, PASSWORD_DEFAULT);
-        $insert_user = "INSERT INTO users (username, password, email) VALUES ('{$username}', '{$password_hash}', '{$email}')";
-        mysqli_query($conn, $insert_user);
-        mysqli_close($conn);
+        $insert_user_query = "INSERT INTO users (username, password, email) VALUES (:username, :password_hash, :email)";
+        $prep = $conn->prepare($insert_user_query);
+        $prep->bindParam(':username', $username, PDO::PARAM_STR);
+        $prep->bindParam(':password_hash', $password_hash, PDO::PARAM_STR);
+        $prep->bindParam(':email', $email, PDO::PARAM_STR);
+        $prep->execute();
+        $conn = null;
         user_login($email, $password);
     }
 
     # Validates data provided in register form
     function validate_register_form(string $email, string $username, string $password, string $confirm_password){
-        $conn = open_db_connection();
         $data_valid = true;
         $errors = [];
 
         if (strlen($email) > 100){
             $data_valid = false;
             $errors['email_error'] = "This email is too long (max. 100 characters).";
-        } else if(check_if_email_used($conn, $email)){
+        } else if(check_if_email_used($email)){
             $data_valid = false;
             $errors['email_error'] = "This email address is already used.";
         }
@@ -66,7 +133,7 @@
         if (strlen($username) > 25){
             $data_valid = false;
             $errors['username_error'] = "This username is too long (max 25 characters).";
-        } else if(check_if_username_used($conn, $username)){
+        } else if(check_if_username_used($username)){
             $data_valid = false;
             $errors['username_error'] = "This username is already used.";
         }
@@ -84,7 +151,6 @@
             $errors['password_error'] = "Passwords needs to be identical.";
             $errors['confirm_password_error'] = "Passwords need to be identical.";
         }
-        mysqli_close($conn);
         return ['success'=>$data_valid, 'errors'=>$errors];
     }
 ?>
